815e4ff1ad2f6df2f543d2bb5982ebf4a200d897
Nicholas Todoroff <loppy1243@gmail.com>
2018-07-28 15:42:00 -0400

    Add SpinMod.jl

---
+++ src/SpinMod.jl
@@ -0,0 +1,12 @@
+@reexport module SpinMod
+export Spin, SPINUP, SPINDOWN, SPINS
+
+@enum Spin::Bool SPINUP=true SPINDOWN=false
+
+const SPINS = [SPINUP, SPINDOWN]
+
+flip(s::Spin) = Spin(~Bool(s))
+spinup(s::Spin) = Bool(s)
+spindown(s::Spin) = ~Bool(s)
+
+end # module SpinMod
