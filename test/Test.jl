dea468b32ec886d043910d9132b1220f7df0411b
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-01 00:22:08 -0400

    Begin adding tests
    In particular, test that indexing a Basis generates elements in the same
    order as basis()

---
+++ test/Test.jl
@@ -0,0 +1,8 @@
+module Test
+using Base.Test
+include("../src/ManyBody.jl")
+using .ManyBody
+
+include("basisindex.jl")
+
+end
---
+++ test/basisindex.jl
@@ -0,0 +1,11 @@
+function basisindex()
+@testset "Basis Indexing" begin
+    for BASIS in (Bases.Pairing{4},
+                  Bases.PartHole{RefStates.Fermi{2, Bases.Pairing{4}}},
+                  Bases.PHPaired{2, 4})
+        b = basis(BASIS)
+
+        println("Testing basis: ", BASIS)
+        @test all(b[i] == BASIS[i] for i in indices(BASIS))
+    end end
+end
