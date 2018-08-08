3dfe449eedc9d820bcbf819461bc716d2090a30d
Nicholas <todoroff@nscl.msu.edu>
2018-08-08 15:08:07 -0400

    Add Index, Sub methods for isocc()

--- src/Bases/refstates.jl
+++ src/Bases/refstates.jl
@@ -50,10 +50,15 @@ const n_occ = nholes
 #indexh(::Type{<:RefStates.Vacuum}, n::Int) = 0
 #indexh(::Type{RefStates.Fermi{F, SP}}, n::Int) where {F, SP<:Pairing} = SP[n]
 
+isocc(::Type{R}, s<:Index{B}) where B<:AbstractBasis = isocc(R, B[index(s)])
+isocc(::Type{R}, s<:Sub) = isocc(R, s.state)
+
 isocc(::Type{R}, s::SP) where {SP, R<:RefState{SP}} = s in holes(R)
 isocc(::Type{RefStates.Vacuum{SP}}, s::SP) where SP = false
 isocc(::Type{R}, s::SP) where {F, SP<:Pairing, R<:RefStates.Fermi{F, SP}} =
     level(s) <= F
+isocc(::Type{R}, s::Index{B}) where
+     {F, B<:Pairing, R<:RefStates.Fermi{F, <:MaybeIndex{B}}} = index(s) <= 2F
 const ishole = isocc
 
 ispart(R, x) = ~ishole(R, x)
