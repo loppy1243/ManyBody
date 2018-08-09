4e160cf125f2ec14e04cc596783b94a4480948a7
Nicholas <todoroff@nscl.msu.edu>
2018-08-09 15:58:03 -0400

    Fix typos

--- src/Bases/main.jl
+++ src/Bases/main.jl
@@ -18,8 +18,8 @@ Base.firstindex(::Type{B}) where B<:AbstractBasis = 1
 Base.lastindex(::Type{B}) where B<:AbstractBasis = dim(B)
 
 Base.iterate(::Type{B}, i=1) where B<:AbstractBasis = i > dim(B) ? nothing : (B[i], i+1)
-Base.IteratorSize(::Type{B}) where B<:AbstractBasis = HasLength()
-Base.IteratorElType(::Type{B}) where B<:AbstractBasis = HasElType()
+Base.IteratorSize(::Type{B}) where B<:AbstractBasis = Base.HasLength()
+Base.IteratorEltype(::Type{B}) where B<:AbstractBasis = Base.HasEltype()
 
 Base.length(::Type{B}) where B<:AbstractBasis = dim(B)
 Base.eltype(::Type{B}) where B<:AbstractBasis = B
