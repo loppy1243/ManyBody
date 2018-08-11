cbfcdbe963dfb98251c237e22da318a11d73fbc7
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-11 11:47:03 -0400

    Write test for Bases.Generation

--- test/basisgeneration.jl
+++ test/basisgeneration.jl
@@ -1,4 +1,17 @@
-function basisgeneration()
+computed_default(BASIS) = @testset "Computed Default" begin
+    @test Bases.Generation(BASIS) === Bases.Computed()
+    @test basis(BASIS) !== basis(BASIS)
+end
+
+generate_macro(BASIS) = @testset "Bases.@generate" begin
+    @test Bases.Generation(BASIS) === Bases.Generated()
+    @test basis(BASIS) === basis(BASIS)
+end
+
 @testset "Basis Generation" begin
-end; end
-basisgeneration()
+    BASIS = BASES.Pairing{4}
+
+    computed_default(BASIS)
+    Bases.@generate BASIS
+    generate_macro(BASIS)
+end
