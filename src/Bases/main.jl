29f92011488e1aeb08ed93c8ce2e216b498a8a19
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-11 17:42:23 -0400

    Add missing type constraints

--- src/Bases/main.jl
+++ src/Bases/main.jl
@@ -35,7 +35,7 @@ macro generate(T)
     end
 end
 
-basis(::Type{B}) = _basis(B, Generation(B))
+basis(::Type{B}) where B<:AbstractBasis = _basis(B, Generation(B))
 basis(::Type{B}, ::Computed) where B<:AbstractBasis = B[1:dim(B)]
 @generated basis(::Type{B}, ::Generated) where B<:AbstractBasis =
     map(i -> indexbasis(B, i), 1:dim(B))
@@ -46,7 +46,8 @@ _getindex(::Type{B}, ::Computed, ixs::Array) where B<:AbstractBasis =
     map(i -> indexbasis(B, i), ixs)
 _getindex(::Type{B}, ::Computed, r::AbstractRange{Int}) where B<:AbstractBasis =
     map(i -> indexbasis(B, i), r)
-_getindex(::Type{B}, ::Union{Generated, Provided}, ixs...) = basis(B)[ixs...]
+_getindex(::Type{B}, ::Union{Generated, Provided}, ixs...) where B<:AbstractBasis =
+    basis(B)[ixs...]
 
 Base.firstindex(::Type{B}) where B<:AbstractBasis = 1
 Base.lastindex(::Type{B}) where B<:AbstractBasis = dim(B)
