2f27c63b42414c6dd24232663cc5a6acc609fd2e
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-13 16:58:40 -0400

    Update example.jl

--- example.jl
+++ example.jl
@@ -9,27 +9,22 @@ const LEVEL_SPACING = 1
 const SPBASIS = Bases.Pairing{4}
 const REFSTATE = RefStates.Fermi{2, SPBASIS}
 const MBBASIS = Bases.Paired{2, 4}
-#const INDICES = indices(SPBASIS)
-#const PARTS = parts(REFSTATE)
-#const HOLES = holes(REFSTATE)
 
-f(g) = (X, Y) -> sum(SPBASIS) do p
-    sgn, Y2 = A(p', p)(Y)
-    (LEVEL_SPACING*(level(p)-1))*sgn*(X'Y2)
+f(g) = CFunctionOperator{1, MBBASIS}() do X
+    sum(SPBASIS) do p
+        LEVEL_SPACING*(level(p)-1)*A(p', p)(X)
+    end
 end
 
-V(g) = (X, Y) -> sum(cartesian_pow(1:nlevels(SPBASIS), Val{2})) do ls
-    p, q, r, s = SPBASIS.((ls[1], ls[1], ls[2], ls[2]),
-                          (SPINUP, SPINDOWN, SPINUP, SPINDOWN))
-
-    sgn, Y2 = A(p', q', s, r)(Y)
-    -g/2*sgn*(X'Y2)
+V(g) = CFunctionOperator{1, MBBASIS}() do X
+    -g/2 * sum(cartesian_pow(1:nlevels(SPBASIS), Val{2})) do ls
+        p, q, r, s = SPBASIS.((ls[1], ls[1], ls[2], ls[2]),
+                              (SPINUP, SPINDOWN, SPINUP, SPINDOWN))
+        A(p', q', s, r)(X)
+    end
 end
 
-H(g) = Operator{2, SPBASIS}() do X, Y
-    f(g)(X, Y) + V(g)(X, Y)
-end
-
+H(g) = f(g) + V(g)
 main() = tabulate(H(1.0))
 
 end # module Exec
