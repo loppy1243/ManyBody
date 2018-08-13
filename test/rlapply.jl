d958daad5cc673505c4b829c4384d2b11da8de07
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-13 16:58:06 -0400

    Update rlapply test

--- test/rlapply.jl
+++ test/rlapply.jl
@@ -7,35 +7,35 @@ function rlapply()
                                 (SPINUP, SPINDOWN, SPINUP, SPINUP, SPINDOWN, SPINDOWN))
 
     @testset "One Body" begin for X in MBBASIS
-        X2 = convert(Bases.Slater, X)
-        @test A(p, p)(X) == (0, States.ZERO)
-        @test A(q, q)(X) == (0, States.ZERO)
-        @test A(p', p')(X) == (0, States.ZERO)
-        @test A(q', q')(X) == (0, States.ZERO)
-        @test A(p', p)(X) == (p in X, p in X ? X2 : States.ZERO)
-        @test A(q', q)(X) == (q in X, q in X ? X2 : States.ZERO)
-        @test A(p, p')(X) == (1 - (p in X), p in X ? States.ZERO : X2)
-        @test A(q, q')(X) == (1 - (q in X), q in X ? States.ZERO : X2)
+#        X2 = convert(Bases.Slater, X)
+        @test A(p, p)(X) == ZeroState()
+        @test A(q, q)(X) == ZeroState()
+        @test A(p', p')(X) == ZeroState()
+        @test A(q', q')(X) == ZeroState()
+        @test A(p', p)(X) == (p in X) * (p in X ? X : ZeroState())
+        @test A(q', q)(X) == (q in X) * (q in X ? X : ZeroState())
+        @test A(p, p')(X) == (1 - (p in X)) * (p in X ? ZeroState() : X)
+        @test A(q, q')(X) == (1 - (q in X)) * (q in X ? ZeroState() : X)
 
         if p != q && !(p in X) || !(q in X)
-            @test A(p, q)(X) == (0, States.ZERO)
-            @test A(q, p)(X) == (0, States.ZERO)
+            @test A(p, q)(X) == ZeroState()
+            @test A(q, p)(X) == ZeroState()
         end
         if p != q && p in X || q in X
-            @test A(p', q')(X) == (0, States.ZERO)
-            @test A(q', p')(X) == (0, States.ZERO)
+            @test A(p', q')(X) == ZeroState()
+            @test A(q', p')(X) == ZeroState()
         end
     end end
 
     @testset "Two Body" begin
-        @test A(p, q, r, s)(MBBASIS[1]) == (0, States.ZERO)
+        @test A(p, q, r, s)(MBBASIS[1]) == ZeroState()
         X = Bases.Slater(SPBASIS.((1, 2, 3, 3),
                                   (SPINDOWN, SPINDOWN, SPINDOWN, SPINUP)))
-        @test A(p, q', r, s')(MBBASIS[1]) == (-1, X)
+        @test A(p, q', r, s')(MBBASIS[1]) == -X
         X = Bases.Slater(SPBASIS.((1, 2), (SPINDOWN, SPINDOWN)))
-        @test A(p, q, r, q')(MBBASIS[1]) == (-1, X)
+        @test A(p, q, r, q')(MBBASIS[1]) == -X
         X = Bases.Slater(SPBASIS.((1, 2, 3, 4), (SPINUP, SPINDOWN, SPINDOWN, SPINDOWN)))
-        @test A(t', r, u, q')(MBBASIS[1]) == (-1, X)
+        @test A(t', r, u, q')(MBBASIS[1]) == -X
     end
 end; nothing end
 rlapply()
