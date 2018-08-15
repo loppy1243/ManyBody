445bfb9b90b9a1cac8f6a945f1f849f3261dbcb8
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-14 20:42:39 -0400

    Add methods for Sub{} and Inner{}

--- src/Bases/pairing.jl
+++ src/Bases/pairing.jl
@@ -13,22 +13,29 @@ struct Pairing{Levels} <: AbstractBasis
 end
 
 flipspin(p::Pairing) = typeof(p)(level(p), flip(spin(p)))
+flipspin(p::Sub{<:Pairing}) = typeof(p)(flipspin(p))
+flipspin(p::Index{<:Pairing}) = typeof(p)(flipspin(p))
 
 Base.:(==)(p1::Pairing{L}, p2::Pairing{L}) where L =
     p1.level == p2.level && p1.spin == p2.spin
 
 index(sp::Pairing) = 2(sp.level-1) + 1 + Bool(sp.spin)
 
-function indexbasis(::Type{Pairing{L}}, pn::Int) where L
-    spin_num = Bool(1 - pn % 2)
-    level = div(pn - spin_num - 1, 2) + 1
+index_to_spin(i) = Spin(Bool(1 - i % 2))
+index_to_level(i) = div(i - Bool(index_to_spin(i)) - 1, 2) + 1
 
-    Pairing{L}(level, Spin(spin_num))
-end
+indexbasis(::Type{Pairing{L}}, pn::Int) where L =
+    Pairing{L}(index_to_spin(pn), index_to_level(i))
 
 nlevels(::Type{Pairing{L}}) where L = L
+nlevels(::Type{<:Sub{Pairing{L}}}) where L = L
+nlevels(::Type{Index{Pairing{L}}}) where L = L
 level(sp::Pairing) = sp.level
+level(sp::Sub{<:Pairing}) = sp.state.level
+level(sp::Index{<:Pairing}) = index_to_level(sp.index)
 spin(sp::Pairing) = sp.spin
+spin(sp::Sub{<:Pairing}) = sp.state.spin
+spin(sp::Index{<:Pairing}) = index_to_level(sp.index)
 
 dim(::Type{Pairing{L}}) where L = 2L
 
