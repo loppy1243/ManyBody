832dfbebe17076efba49d16f1ab0827dbf623b96
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-16 18:46:51 -0400

    Remove unnecessary method

--- src/Bases/iter.jl
+++ src/Bases/iter.jl
@@ -4,8 +4,6 @@ _getindex(::Type{B}, ::Computed, ixs::Array) where B<:AbstractBasis =
     map(i -> indexbasis(B, i), ixs)
 _getindex(::Type{B}, ::Computed, r::AbstractRange{Int}) where B<:AbstractBasis =
     map(i -> indexbasis(B, i), r)
-_getindex(::Type{B}, ::Union{Generated, Provided}, ixs...) where B<:AbstractBasis =
-    basis(B)[ixs...]
 
 Base.firstindex(::Type{B}) where B<:AbstractBasis = 1
 Base.lastindex(::Type{B}) where B<:AbstractBasis = dim(B)
