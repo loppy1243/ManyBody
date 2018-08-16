acf0b3965ce74dbf3bec08db069d0b550a1455c4
Nicholas Todoroff <loppy1243@gmail.com>
2018-08-15 23:46:08 -0400

    Remove unnecessary methods

--- src/Bases/indexbasis.jl
+++ src/Bases/indexbasis.jl
@@ -9,16 +9,14 @@ Base.:(==)(a::MaybeIndex{B}, b::MaybeIndex{B}) where B<:AbstractBasis = index(a)
 Base.promote_rule(::Type{Index{B}}, ::Type{B}) where B<:AbstractBasis = B
 
 innertype(::Type{Index{B}}) where B<:AbstractBasis = B
-inner(i::Index{B}) where B<:AbstractBasis = B[i.index]
+inner(i::Index) = innertype(i)[i.index]
 
 index(s::Index) = s.index
 indexbasis(::Type{S}, s::Int) where S<:Index = S(s)
-basis(::Type{Index{B}}) where B = indexes(B)
-dim(::Type{Index{B}}) where B = dim(B)
+dim(B::Type{<:Index}) = dim(innertype(B))
 
-Base.convert(::Type{Index{B}}, s::B) where B<:AbstractBasis = Index{B}(index(s))
-Base.convert(::Type{Index}, s::AbstractBasis) = Index{typeof(s)}(index(s))
-Base.convert(::Type{B}, s::Index{B}) where B<:AbstractBasis = B(s)
+Base.convert(::Type{<:Index}, s::AbstractBasis) = Index(s)
+#Base.convert(::Type{B}, s::Index{B}) where B<:AbstractBasis = B(s)
 Base.convert(::Type{C}, s::Index{B}) where {C<:AbstractBasis, B<:C} = B(s)
 
 B(s::Index{B}) where B<:AbstractBasis = B[s.index]
