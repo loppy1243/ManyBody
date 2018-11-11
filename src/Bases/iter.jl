faa800cb7ad9b23db0f5696b4cbec7910f2d91a9
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-10 19:23:09 -0500

    Add AbstractBasis type and make TensorBasis <: AbstractBasis

--- src/Bases/iter.jl
+++ src/Bases/iter.jl
@@ -1,18 +1,18 @@
-Base.getindex(B::Type{<:TensorBasis}, ixs...) = indexbasis(B, ixs...)
-Base.to_index(b::TensorBasis) = index(b)
+Base.getindex(B::Type{<:AbstractBasis}, ixs...) = indexbasis(B, ixs...)
+Base.to_index(b::AbstractBasis) = index(b)
 
 Base.eachindex(B::Type{<:TensorBasis}) = CartesianIndices(axes(B))
-Base.firstindex(B::Type{<:TensorBasis}) = CartesianIndex(ones(rank(B))...)
+Base.firstindex(B::Type{<:TensorBasis}) = CartesianIndex(ones(Int, rank(B))...)
 Base.lastindex(B::Type{<:TensorBasis}) = CartesianIndex(fulldims(B))
 
 Base.IteratorSize(::Type{B}) where B<:Type{<:TensorBasis} = Base.HasShape{rank(B)}()
 Base.axes(B::Type{<:TensorBasis}) = map(d -> 1:d, fulldims(B))
 Base.length(B::Type{<:TensorBasis}) = prod(fulldims(B))
 
-Base.IteratorEltype(::Type{<:Type{<:TensorBasis}}) = Base.HasEltype()
-Base.eltype(B::Type{<:TensorBasis}) = B
+Base.IteratorEltype(::Type{<:Type{<:AbstractBasis}}) = Base.HasEltype()
+Base.eltype(B::Type{<:AbstractBasis}) = B
 
-function Base.iterate(B::Type{<:TensorBasis}, st=(eachindex(B),))
+function Base.iterate(B::Type{<:AbstractBasis}, st=(eachindex(B),))
     (x = iterate(st...)) === nothing && return nothing
     (I, inner_st) = x
 
--- src/Bases/main.jl
+++ src/Bases/main.jl
@@ -14,16 +14,18 @@ using Reexport: @reexport
 #abstract type AbstractIndex{B<:ConcreteBasis} <: LeafBasis end
 #const MaybeIndex{B<:ConcreteBasis} = Union{B, AbstractIndex{B}}
 
-abstract type TensorBasis{Rank} end
+abstract type AbstractBasis
+abstract type TensorBasis{Rank} <: AbstractBasis end
 const Basis = TensorBasis{1}
+#abstract type FockBasis{SPBasis<:TensorBasis} <: AbstractBasis end
 
 rank(::Type{<:TensorBasis{N}}) where N = N
-rank(b::TensorBasis) = rank(typeof(b))
+rank(b::AbstractBasis) = rank(typeof(b))
 dim(B::Type{<:TensorBasis}) = prod(fulldims(B))
 # Good way to do this?
 #fulldims(B::Type{<:Basis}) = (dim(B),)
 
-Base.:(==)(x::B, y::B) where B<:TensorBasis = index(x) == index(y)
+Base.:(==)(x::B, y::B) where B<:AbstractBasis = index(x) == index(y)
 
 include("iter.jl")
 include("pairing.jl")
@@ -33,28 +35,6 @@ include("product.jl")
 ### Update Line
 ##############################################################################################
 
-include("refstates.jl")
 include("slater.jl")
-
-@defSub Paired{F, L} <: Slater{Pairing{L}} begin
-    SP = Pairing{L}
-    ref = Slater{SP}(SP(l, s) for l = 1:F, s in SPINS)
-
-    ret = [ref]
-    for ph = 1:L-F
-        for lhs in combinations(1:F, ph), lps in combinations(F+1:L, ph)
-            state = deepcopy(ref)
-            for (lh, lp) in zip(lhs, lps), s in SPINS
-                annihil!(state, SP(lh, s))
-                create!(state, SP(lp, s))
-            end
-
-            push!(ret, state)
-        end
-    end
-
-    ret
-end
-IndexType(::Type{<:Paired}) = IndexTypes.Linear()
-
+include("refstates.jl")
 end # module States
