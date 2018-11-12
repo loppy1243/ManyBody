4eff707c38fc0ebdf1098bb97b88b6fb7c9e73b6
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-11 21:41:52 -0500

    Remove unnecessary includes from src/ManyBody.jl

--- src/ManyBody.jl
+++ src/ManyBody.jl
@@ -3,20 +3,10 @@ export Bases, States, Operators, basistype, reptype
 
 using Reexport: @reexport
 
-include("util.jl")
-
 abstract type AbstractState end
-
-basistype(x) = basistype(typeof(x))
-@disallow basistype(::Type)
-
-#reptype(x) = reptype(typeof(x))
-#@disallow reptype(::Type)
-reptype() = nothing
 
 include("SpinMod.jl")
 include("Bases/main.jl")
-include("States.jl")
 include("Operators.jl")
 
 end # module ManyBody
