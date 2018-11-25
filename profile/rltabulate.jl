2406b81625419c493df8ce3a07cd75c87423ee3d
Nicholas Todoroff <loppy1243@gmail.com>
2018-11-24 23:49:42 -0500

    Update profile/rltabulate.jl

--- profile/rltabulate.jl
+++ profile/rltabulate.jl
@@ -2,9 +2,9 @@ using ManyBody, Profile, ProfileView
 
 function rltabulate_profile()
     # For precompilation
-    tabulate(Hamiltonians.pairing(1, 0.5), Float64, Bases.Paired{4, 4})
+    tabulate(Hamiltonians.pairing(1, 0.5), Array{Float64, 2}, Bases.Paired{4, 4})
     
     Profile.init()
-    @profile tabulate(Hamiltonians.pairing(1, 0.5), Float64, Bases.MBPairing{4, 4})
+    @profile tabulate(Hamiltonians.pairing(1, 0.5), Array{Float64, 2}, Bases.MBPairing{4, 4})
     ProfileView.view()
 end
