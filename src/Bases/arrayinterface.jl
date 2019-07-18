98335318e07a6ca930826a32263471e045a85e2b
Nicholas Todoroff <loppy1243@gmail.com>
2019-07-18 18:56:55 -0400

    no-tabulate: Add eachindex method over AbstractBasis

--- src/Bases/arrayinterface.jl
+++ src/Bases/arrayinterface.jl
@@ -1,21 +1,54 @@
-Base.IteratorSize(::Type{B}) where B<:Type{<:TensorBasis} = Base.HasShape{rank(B)}()
-Base.IteratorSize(::Type{B}) where B<:Type{<:AbstractBasis} = Base.HasLength()
-Base.axes(B::Type{<:TensorBasis}) = map(d -> 1:d, fulldims(B))
-Base.length(B::Type{<:TensorBasis}) = prod(fulldims(B))
-Base.length(B::Type{<:AbstractBasis}) = dim(B)
+# If `IndexStyle(Basis) == IndexLinear()`:
+#     Must implement
+#           `index(::Basis)::Int`
+#           `indexbasis(::Type{Basis}, ::Int)::Basis`
+# If `IndexStyle(Basis) == IndexCartesian()`:
+#       Must implement
+#           `index(::Basis)::CartesianIndex`
+#           `indexbasis(::Type{Basis}, ::Vararg{Int})::Basis`
+# By default, `IndexStyle(::Type{<:TensorBasis}) == IndexCartesian()`.
 
-Base.size(B::Type{<:TensorBasis}) = fulldims(B)
-Base.size(B::Type{<:AbstractBasis}) = (dim(B),)
+struct Flat{B<:AbstractBasis} <: AbstractVector{B} end
+Flat(B::Type{<:AbstractBasis}) = Flat{B}()
+Flat(s::Shaped) = Flat{eltype(s)}()
 
-Base.IteratorEltype(::Type{<:Type{<:AbstractBasis}}) = Base.HasEltype()
-Base.eltype(B::Type{<:AbstractBasis}) = B
-#Base.eltype(B::Type{<:Sub}) = supbasis(B)
-Base.eltype(B::Type{<:Indexer{SB}}) where SB<:Sub = supbasis(SB)
-Base.eltype(B::Type{<:LinearIndexer{SB}}) where SB<:Sub = supbasis(SB)
-
-function Base.iterate(B::Type{<:AbstractBasis}, st=(eachindex(B),))
-    (x = iterate(st...)) === nothing && return nothing
-    (I, inner_st) = x
-
-    (indexer(B)[I], (st[1], inner_st))
+struct Shaped{N, B<:TensorBasis{N}} <: AbstractArray{B, N}
+    Shaped{B}() where B<:TensorBasis = new{rank(B), B}()
 end
+Shaped(B::Type{<:TensorBasis}) = Shaped{B}()
+Shaped(f::Flat{<:TensorBasis}) = Shaped{eltype(f)}()
+
+Base.size(s::Flat) = (dim(eltype(f)),)
+Base.size(s::Shaped) = fulldims(eltype(f))
+
+elems(B::Type{<:AbstractBasis}) = Flat(B)
+elems(B::Type{<:TensorBasis}) = Shaped(B)
+Base.eachindex(B::Type{<:AbstractBasis}) = eachindex(elems(B))
+Base.vec(a::Shaped) = Flat(a)
+Base.vec(a::Flat) = a
+Base.+(B::Type{<:AbstractBasis}) = elems(B)
+Base.-(B::Type{<:AbstractBasis}) = vec(elems(B))
+Base.+(b::AbstractBasis) = index(b)
+Base.-(b::AbstractBasis) = _minus(b, IndexStyle(b))
+_minus(b::AbstractBasis, ::IndexLinear) = index(b)
+_minus(b::AbstractBasis, ::IndexCartesian) = LinearIndices(elems(typeof(b)))[index(b)]
+
+indexbasis(B::Type{<:TensorBasis{N}}, I::NTuple{N, Int}) where N = indexbasis(B, I...)
+indexbasis(B::Type{<:TensorBasis{N}}, I::CartesianIndex{N}) where N =
+    indexbasis(B, Tuple(I)...)
+
+## Could possibly be more efficient to index a TensorBasis linearly, so we account for that.
+Base.IndexStyle(::Type{<:AbstractBasis}) = IndexLinear()
+Base.IndexStyle(::Type{<:TensorBasis}) = IndexCartesian()
+Base.IndexStyle(::Type{<:Flat}) = IndexLinear()
+Base.IndexStyle(S::Type{<:Shaped}) = IndexStyle(eltype(S))
+
+Base.getindex(a::Flat, i::Int) = indexbasis(eltype(a), i)
+Base.getindex(a::Flat{<:TensorBasis}, i::Int) = _getindex(a, i, IndexStyle(eltype(a)))
+Base.getindex(a::Shaped, I::Int...) = _getindex(a, I, IndexStyle(a))
+_getindex(a::Flat{<:TensorBasis}, i::Int, ::IndexLinear) = indexbasis(eltype(a), i)
+_getindex(a::Flat{<:TensorBasis}, i::Int, ::IndexCartesian) =
+    indexbasis(eltype(a), CartesianIndices(Shaped(a))[i])
+_getindex(a::Shaped, I::Tuple{Int}, ::IndexLinear) = indexbasis(eltype(a), I[1])
+_getindex(a::Shaped{N}, I::NTuple{N, Int}, ::IndexCartesian) where N =
+    indexbasis(eltype(a), I)
